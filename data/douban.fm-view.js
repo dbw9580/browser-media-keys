30b503e305e96af9b8dbc44d58bade1519006577
dbw9580 <dbw9580@live.com>
2017-11-04 19:58:01 +0800

    fix douban.fm-view.js xpath

--- data/douban.fm-view.js
+++ data/douban.fm-view.js
@@ -3,6 +3,6 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
-MediaKeys.playButton = "//label[@title='播放']/svg[contains(@class, 'icon') and not(contains(@class, 'icon-pause'))]";
-MediaKeys.pauseButton = "//label[@title='播放']/svg[contains(@class, 'icon') and contains(@class, 'icon-pause')]";
-MediaKeys.skipButton = "//label[@title='下一首']/svg[contains(@class, 'icon')]";
+MediaKeys.playButton = "//label[@title='播放']/*[1][contains(@class, 'icon') and not(contains(@class, 'icon-pause'))]";
+MediaKeys.pauseButton = "//label[@title='暂停']/*[1][contains(@class, 'icon') and contains(@class, 'icon-pause')]";
+MediaKeys.skipButton = "//label[@title='下一首']/*[1][contains(@class, 'icon')]";
